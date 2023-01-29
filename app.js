ce443f5b730b81834e377e333042588e35e77fc2
nonsoikenwa1994 <54154122+nonsoikenwa1994@users.noreply.github.com>
2023-01-29 12:48:57 +0100

    New changes

---
+++ app.js
@@ -0,0 +1,47 @@
+var faker = require('@faker-js/faker');
+// const { appendFile } = require('fs');
+var mysql = require('mysql');
+ var connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    database : 'sql_test',
+    password: "Nonsoikenwa#1994",
+      insecureAuth : true
+    });
+
+    // connection.connect(function(err) {
+    //   if (err) throw err;
+    //   console.log("Connected!");
+    // });
+
+//     database : 'ig_clone'
+//  });
+// var q = 'select count(*) as total from users';
+// connection.query(q, function (error, results, fields) {
+// if (error) throw error;
+// console.log(results[0].total);
+    // console.log(results[0].time);
+    // console.log(results[0].date);
+    // console.log(results[0].now);
+// });
+// Inserting data take 1
+var q = 'INSERT INTO users (email) VALUES ("js3fe11du_the_og@gmail.com")';
+ 
+connection.query(q, function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+});
+
+// inserting data take 2
+// var person = {
+//   email: faker.internet.email()
+   // created_at: faker.date.past()
+// };
+ 
+// var end_result = connection.query('INSERT INTO users SET ?', person, function(err, result) {
+//   if (err) throw err;
+//   console.log(result);
+// });
+
+
+//connection.end();
